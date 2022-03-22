0a7fb5e366023a30a0125b35cc5472f206ecfbba
jiwonp6 <pwldnjs2806@naver.com>
2022-03-22 18:04:17 +0900

    0322.2

--- source/01_java/ch11_abstractclass/src/com/lec/ex04_Car/TestMain.java
+++ source/01_java/ch11_abstractclass/src/com/lec/ex04_Car/TestMain.java
@@ -4,7 +4,10 @@ import com.lec.cons.CarSpec;
 
 public class TestMain {
 	public static void main(String[] args) {
-		Car car = new LowGradeCar(CarSpec.RED_CAR, CarSpec.TIRE_WIDE, CarSpec.DISPLACEMENT_1000, CarSpec.HANDLE_POWER);
+//		Car car = new LowGradeCar(CarSpec.RED_CAR, CarSpec.TIRE_NORMAL, 
+//										CarSpec.DISPLACEMENT_1000, CarSpec.HANDLE_NORMAL);
+		Car car = new HighGradeCar(CarSpec.WHITE_CAR, CarSpec.TIRE_WIDE, 
+										CarSpec.DISPLACEMENT_2200, CarSpec.HANDLE_POWER);
 	car.getSpec();
 	}
 }
