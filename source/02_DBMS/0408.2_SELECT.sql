408f9139b2159738672185b7622b6edbd358fe07
Jiwon <101315853+jiwonp6@users.noreply.github.com>
2022-04-11 13:58:02 +0900

    Add files via upload

--- source/02_DBMS/0408.2_SELECT.sql
+++ source/02_DBMS/0408.2_SELECT.sql
@@ -71,35 +71,68 @@ SELECT DEPTNO FROM EMP;
 SELECT DISTINCT JOB FROM EMP;
 SELECT DISTINCT DEPTNO FROM EMP;
 
+--8. SQL 연산자(BETWEEN, IN, LIKE, IS NULL)
+    --(1)BETWEEN
+        --EX. SAL이 1500이상 3000이하인 사번 이름 급여
+SELECT EMPNO, ENAME, SAL FROM EMP WHERE SAL>=1500 AND SAL<=3000;
+SELECT EMPNO, ENAME, SAL FROM EMP WHERE SAL BETWEEN 1500 AND 3000;
+SELECT EMPNO, ENAME, SAL FROM EMP WHERE SAL BETWEEN 3000 AND 1500;  --X
+        --EX. 1500미만, 3000초과하는 모든 필드 출력
+SELECT * FROM EMP WHERE SAL NOT BETWEEN 1500 AND 3000;
+
 --★ 연습문제 꼭 풀기!
---1. emp 테이블의 구조 출력
+    --1. emp 테이블의 구조 출력 (오라클에서만 가능)
 DESC EMP;
---2. emp 테이블의 모든 내용을 출력 
+
+    --2. emp 테이블의 모든 내용을 출력 
 SELECT * FROM EMP;
---3. 현 scott 계정에서 사용가능한 테이블 출력
-SELECT * FROM TAB;
---4. emp 테이블에서 사번, 이름, 급여, 업무, 입사일 출력
+
+    --3. 현 scott 계정에서 사용가능한 테이블 출력
+SHOW USER;  --SCOTT으로 접속돼있는지 확인
+SELECT * FROM TAB; --현재 계정이 가지고 있는 테이블
+
+    --4. emp 테이블에서 사번, 이름, 급여, 업무, 입사일 출력
 SELECT EMPNO, ENAME, SAL, JOB, HIREDATE FROM EMP;
---5. emp 테이블에서 급여가 2000미만인 사람의 사번, 이름, 급여 출력
+
+    --5. emp 테이블에서 급여가 2000미만인 사람의 사번, 이름, 급여 출력
 SELECT EMPNO, ENAME, SAL FROM EMP WHERE SAL < 2000;
---6. 입사일이 81/02이후에 입사한 사람의 사번, 이름, 업무, 입사일 출력
+
+    --6. 입사일이 81/02이후에 입사한 사람의 사번, 이름, 업무, 입사일 출력
 SELECT EMPNO, ENAME, JOB, HIREDATE FROM EMP WHERE HIREDATE >= '81/02/01' ;
---7. 업무가 SALESMAN인 사람들 모든 자료 출력
+    --#날짜설정(YYYY 년도4자리/RR 년도2자리/M월/D일)
+ALTER SESSION SET NLS_DATE_FORMAT = 'MM-DD-RR' 
+    --#날짜형->문자형으로 변환하는 함수 : TO_CHAT(날짜형데이터, 패턴)
+    --#문자형->날짜형으로 변환하는 함수 : TO_DATE(문자형데이터, 패턴)
+SELECT EMPNO, ENAME, JOB, HIREDATE FROM EMP 
+    WHERE TO_CHAR(HIREDATE, 'RR/MM/DD') >= '81/02/01' ;  --단일행함수(4장 참조)
+SELECT EMPNO, ENAME, JOB, HIREDATE FROM EMP 
+    WHERE HIREDATE >= TO_DATE('81/02/01', 'RR/MM/DD') ;  --단일행함수(4장 참조)
+ALTER SESSION SET NLS_DATE_FORMAT = 'RR/MM/DD'  --원상복귀^^
+
+    --7. 업무가 SALESMAN인 사람들 모든 자료 출력
 SELECT * FROM EMP WHERE JOB = 'SALESMAN';
---8. 업무가 CLERK이 아닌 사람들 모든 자료 출력
+
+    --8. 업무가 CLERK이 아닌 사람들 모든 자료 출력
 SELECT * FROM EMP WHERE JOB != 'CLERK';
---9. 급여가 1500이상이고 3000이하인 사람의 사번, 이름, 급여 출력
+SELECT * FROM EMP WHERE JOB <> 'CLERK';
+SELECT * FROM EMP WHERE JOB ^= 'CLERK';
+SELECT * FROM EMP WHERE NOT JOB = 'CLERK';
+
+    --9. 급여가 1500이상이고 3000이하인 사람의 사번, 이름, 급여 출력
 SELECT EMPNO, ENAME, SAL FROM EMP WHERE SAL>=1500 AND SAL <= 3000;
---10. 부서코드가 10번이거나 30인 사람의 사번, 이름, 업무, 부서코드 출력
+
+    --10. 부서코드가 10번이거나 30인 사람의 사번, 이름, 업무, 부서코드 출력
 SELECT EMPNO, ENAME, JOB, DEPTNO FROM EMP WHERE DEPTNO = 10 OR DEPTNO = 30;
---11. 업무가 SALESMAN이거나 급여가 3000이상인 사람의 사번, 이름, 업무, 부서코드 출력
+
+    --11. 업무가 SALESMAN이거나 급여가 3000이상인 사람의 사번, 이름, 업무, 부서코드 출력
 SELECT EMPNO, ENAME, JOB, DEPTNO FROM EMP WHERE JOB = 'SALESMAN' OR SAL >= 3000;
---12. 급여가 2500이상이고 업무가 MANAGER인 사람의 사번, 이름, 업무, 급여 출력
+
+    --12. 급여가 2500이상이고 업무가 MANAGER인 사람의 사번, 이름, 업무, 급여 출력
 SELECT EMPNO, ENAME, JOB, DEPTNO FROM EMP WHERE JOB = 'MANAGER' AND SAL >= 2500;
---13.“ename은 XXX 업무이고 연봉은 XX다” 스타일로 모두 출력(연봉은 SAL*12+COMM)
-SELECT ENAME || '은 ' || JOB ||'업무이고 연봉은 ' ||(SAL*12)||'이다' FROM EMP;
 
-
+    --13.“ename은 XXX 업무이고 연봉은 XX다” 스타일로 모두 출력(연봉은 SAL*12+COMM)
+SELECT ENAME || '은 ' || JOB ||'업무이고 연봉은 ' ||(SAL*12+NVL(COMM,0))||'이다' FROM EMP;
+    --(COMM은 상여금, NULL일 수 있기때문에 NVL붙여줘야함)
 
 
 
